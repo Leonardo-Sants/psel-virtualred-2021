e222e4c9678d97f0f32391e906fffdb675ed2b75
Leonardo dos Santos <Leonardo-Sants@github.com>
2021-06-29 21:28:06 -0300

    Instalando o Vue-quill-editor

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import router from './routes/router.js'
+import VueQuillEditor from 'vue-quill-editor'
+
 
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueQuillEditor)
 
 Vue.config.productionTip = false
 
